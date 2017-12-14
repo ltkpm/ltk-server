68a0a6cb2213ffc6ee6aa876dd1198fef5cd52a6
Giulio Fagioli <fagioli.giulio@gmail.com>
2017-12-14 15:06:38 +0100

    Refactor some code on db class for general purpose

--- Database/db.js
+++ Database/db.js
@@ -1,56 +1,52 @@
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
-const fs = require('fs')
+const fs = require("fs");
 
-var Database = function () {
-    this.db_path = 'Database/database.json'
-    this.existing = false
-    this.adapter
-    this.db
-}
+var Database = function() {
+  this.db_path = "Database/";
+  this.existing = false;
+  this.adapter;
+  this.db;
+};
 
-Database.prototype.init = function (database_path) {
-    if(database_path != undefined)
-        this.db_path = database_path
+Database.prototype.init = function(db_name) {
+  this.db_path += db_name;
+  fs.exists(this.db_path, exists => {
+    console.log(exists ? "it's there" : "no db!");
+    if (exists) {
+      this.existing = true;
+      this.initAdapter();
+    } else {
+      this.createDb();
+      this.initAdapter();
+      this.initDatabase();
+    }
+  });
+};
 
-    fs.exists(this.db_path, (exists) => {
-        console.log(exists ? 'it\'s there' : 'no db!');
-        if (exists){
-            this.existing = true
-            this.initAdapter()
-        } 
-        else{
-            this.createDb()
-            this.initAdapter()
-            this.initDatabase()
-        }
-    });
-}
-
-Database.prototype.createDb =  function () {
-    fs.writeFile(this.db_path, "", function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("The file was saved!");
-    }); 
-}
+Database.prototype.createDb = function() {
+  fs.writeFile(this.db_path, "", function(err) {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("The file was saved!");
+  });
+};
 
 Database.prototype.initAdapter = function() {
-   this.adapter = new FileSync(this.db_path);
-   this.db = low(this.adapter);
+  this.adapter = new FileSync(this.db_path);
+  this.db = low(this.adapter);
 };
 
 Database.prototype.initDatabase = function() {
   this.db.defaults().write(); //Rimane da mettere i default
 };
 
-Database.prototype.addPackage = function(package) {
+Database.prototype.addElement = function(node,package) {
   this.db
-    .get("packages")
+    .get(node)
     .push(package)
     .write();
 };
 
-
-module.exports.Database = Database
+module.exports.Database = Database;
