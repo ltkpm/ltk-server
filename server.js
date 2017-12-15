0257ee90e6f1eb252764b4417e77f729398086da
Giulio Fagioli <fagioli.giulio@gmail.com>
2017-12-15 18:37:41 +0100

    Added Model and test apis with some fake data

--- server.js
+++ server.js
@@ -2,20 +2,36 @@
 
 const fastify = require('fastify')()
 const Database = require('./Database/db.js').Database
+const Validation = require('./Validation/validation.js').Validation
+const DefaultSchema = require("./Model/DefaultSchema.js").DefaultSchema
 
-var database = new Database()
-database.init()
+
+var repository_db = new Database()
+var validator = new Validation()
+var default_schema = new DefaultSchema()
+var fakeRepo = {
+  name: 'pippo',
+  version: '1',
+  type: 'npm',
+  url: 'emfwoefnwe',
+  commit: 'jwfef'
+}
+repository_db.init('repos', default_schema.repository_schema);
 
 fastify.get("/api", async (request, reply) => {
+  console.log("d")
+  repository_db.addElement('repos', fakeRepo)
   return { Hello: "No buono" };
+  
 });
 
 
 fastify.get("/api/all", async (request, reply) => {
-  
+  return repository_db.getAll()
 });
 
 fastify.listen(3000, function(err) {
   if (err) throw err;
   fastify.log.info(`server listening on ${fastify.server.address().port}`);
 });
+
