c2f12a00f8d0c5683d475117f1bfececcdf5d449
Giulio Fagioli <fagioli.giulio@gmail.com>
2018-01-04 23:40:23 +0100

    Set name and type of Repository model to lower case

--- Model/repository.js
+++ Model/repository.js
@@ -2,9 +2,9 @@ const hasha = require('hasha')
 
 class Repository {
     constructor(repository){
-        this.name = repository.name
+        this.name = repository.name.toLowerCase()
         this.url = repository.url
-        this.type = repository.type
+        this.type = repository.type.toLowerCase()
         this.commit = repository.commit
         this.version = repository.version
         this.hash = hasha(repository.name + repository.version)
